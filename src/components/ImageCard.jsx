5d5400681679006baf580004cc116d5e43c3e202
Augusto Recalde <arecalde@fadelsa.com.ar>
2025-03-17 15:57:51 -0300

    feat: agregar componente ImageCard y funcionalidad de carga de álbumes

---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+const ImageCard  = ({ imageUrl, title, description }) => {
+  return (
+    <div className="max-w-sm bg-white rounded-xl shadow-lg overflow-hidden">
+      <div className="h-56 overflow-hidden">
+        <img className="w-full h-full object-cover" src={imageUrl} alt={title} />
+      </div>
+      <div className="p-4">
+        <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
+        <p className="text-gray-600 mt-2">{description}</p>
+      </div>
+    </div>
+  );
+}
+
+ImageCard.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+};
+
+ImageCard.defaultProps = {
+  description: "Sin descripción disponible.",
+};
+
+export default ImageCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import { listAlbums } from "../services/services";
+import ImageCard from "../components/ImageCard";
 
 const Home = () => {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    Promise.all([listAlbums()])
+      .then((res) => {
+        const [albums] = res;
+        const { rows } = albums.data;
+        setAlbums(rows);
+        console.log(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
-    <h1>Home</h1>
+    <>
+        <section className="max-w-6xl mx-auto px-4 py-10">
+      <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">Galería de Imágenes</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      {albums
+          ? albums.map((album) => {
+              return (
+                <ImageCard
+                  key={album.id}
+                  imageUrl={album.url}
+                  title={album.title}
+                  description={album.description}
+                />
+              );
+            })
+          : null}
+      </div>
+    </section>
+    </>
   );
 };
 
